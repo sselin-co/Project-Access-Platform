69cb28986df0eac0cabbc84f2ec57cbfdcfb2738
Xinyi Guo <xguo99@mit.edu>
2020-12-29 12:54:22 +0800

    prevent unauthorized changing app status

--- pages/api/admin/student-applications/[id].js
+++ pages/api/admin/student-applications/[id].js
@@ -1,17 +1,25 @@
 import Admin from '../../utils/Admin';
-
-export default function handler(req, res) {
-    const {
-        query: { id },
-    } = req;
-    if (req.method === 'POST'){
-        const { status } = req.body;
-        try{
-            Admin.updateAppStatus(id, status);
-            res.json(`Status for student ${id} updated successfully.`);
-        }catch (err) {
-            res.error({error: err});
+import { getSession } from 'next-auth/client'
+/**
+ * Update a student's application status
+ * @name POST /api/admin/student-application/id
+ */
+export default async (req, res) => {
+    const session = await getSession({ req });
+    if (session && session.user.name === "admin"){
+        const {
+            query: { id },
+        } = req;
+        if (req.method === 'POST'){
+            const { status } = req.body;
+            try{
+                Admin.updateAppStatus(id, status);
+                res.json(`Status for student ${id} updated successfully.`);
+            }catch (err) {
+                res.error({error: err});
+            }
         }
+    }else{
+        res.send({ error: 'You must be sign in to view the protected content on this page.' });
     }
-
 }
