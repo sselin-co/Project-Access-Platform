4fb5cab86088f15092f78d79daef07e1a1944599
Emmanuel <immanuelidun10@gmail.com>
2020-12-30 03:55:46 -0600

    modified navbar per user

--- components/pa-navbar.js
+++ components/pa-navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useSession, signOut } from 'next-auth/client'
 import styles from "../styles/Home.module.css";
 
 // I know this is in __app.js as a global stylesheet. However, removing this line will center the PA logo for some reason.
@@ -13,24 +12,19 @@ import {
   Button,
   Dropdown,
 } from "react-bootstrap";
-import LoginModal from "./login-modal";
 import SignUpModal from "./signup-modal";
 import Image from "next/image";
 
-export default function PaNavbar() {
-  const [loginModalShow, setLoginModalShow] = React.useState(false);
-  const [signUpModalShow, setSignUpModalShow] = React.useState(false);
-  const [session, loading] = useSession();
 
-  function handleLoginModalHide() {
-    setLoginModalShow(false);
-  }
+export default function PaNavbar() {
+  const [signUpModalShow, setSignUpModalShow] = React.useState(false);
 
   function handleSignUpModalHide() {
     setSignUpModalShow(false);
   }
 
   return (
+    
     <Navbar expand="lg" className={styles.navbar} variant="dark" sticky="top">
       <Navbar.Brand href="/">
         <Image
@@ -44,33 +38,8 @@ export default function PaNavbar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="#home" className={styles.navLink}>
-            Start
-          </Nav.Link>
-          <Nav.Link href="#link" className={styles.navLink}>
-            Bootcamp
-          </Nav.Link>
-          <Nav.Link href="#link" className={styles.navLink}>
-            Resources
-          </Nav.Link>
-          {session && <><Button
-            onClick={signOut}
-            variant="outline-light"
-            className={styles.navbarButton}
-          >
-            Log Out
-          </Button>
           <Button
-            onClick=""
-            variant="warning"
-            className={styles.navbarButton}
-          >
-              {session.user.email}
-          </Button>
-          </>}
-          {!session && 
-          <><Button
-            onClick={() => setLoginModalShow(true)}
+            href="./api/auth/signin/Credentials"
             variant="outline-light"
             className={styles.navbarButton}
           >
@@ -83,12 +52,11 @@ export default function PaNavbar() {
           >
             Sign Up
           </Button>
-          </>}
-
-          <LoginModal show={loginModalShow} onHide={handleLoginModalHide} />
+          
           <SignUpModal show={signUpModalShow} onHide={handleSignUpModalHide} />
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  );
+    
+  )
 }
