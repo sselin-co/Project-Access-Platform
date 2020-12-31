a6385efd0dbad6dc6785a4060188fe01df7c3242
Shaya Selincourt <sselincourt@gmail.com>
2020-12-31 01:36:42 -0800

    Updated Admin.js

--- pages/api/utils/Admin.js
+++ pages/api/utils/Admin.js
@@ -1,42 +1,58 @@
-const Airtable = require('airtable');
-const base = new Airtable({apiKey:process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_ID
+);
 
 class Admin {
-  static async checkAuthentication(email, password){
-    let records = await base('Admin').select({
-      filterByFormula: `email = '${email}'`,
-      fields: ["email", "password"]
-    }).firstPage();
-    if (records.length === 0) {console.log("email does not exist"); return false;}
-    else{
-      return records[0].get("email") === email && records[0].get("password") === password;
+  static async checkAuthentication(email, password) {
+    let records = await base("Admin")
+      .select({
+        filterByFormula: `email = '${email}'`,
+        fields: ["email", "password"],
+      })
+      .firstPage();
+    if (records.length === 0) {
+      console.log("email does not exist");
+      return false;
+    } else {
+      return (
+        records[0].get("email") === email &&
+        records[0].get("password") === password
+      );
     }
   }
-  
-  static async updateAppStatus(id, status){
-    base('Application').update([
-      {
-        "id": `${id}`,
-        "fields": {
-          "status": `${status}`
+
+  static async updateAppStatus(id, status) {
+    await base("Application").update(
+      [
+        {
+          id: `${id}`,
+          fields: {
+            status: `${status}`,
+          },
+        },
+      ],
+      function (err, records) {
+        if (err) {
+          console.error("Error in Admin.js: " + err);
+          return;
         }
+        console.log("Succesful update for " + records[0].get("email"));
       }
-    ], function(err, records){
-      if (err){
-        console.error(err);
-        return;
-      }
-      console.log(records[0].get('email'));
-    })
+    );
   }
 
   static async nameReturn(email, col) {
-    let records = await base('Admin').select({
-      filterByFormula: `email = '${email}'`,
-      fields: ["email", col]
-    }).firstPage();
-    if (records.length === 0) { console.log("email does not exist"); return false; }
-    else {
+    let records = await base("Admin")
+      .select({
+        filterByFormula: `email = '${email}'`,
+        fields: ["email", col],
+      })
+      .firstPage();
+    if (records.length === 0) {
+      console.log("email does not exist");
+      return false;
+    } else {
       return records[0].get(col);
     }
   }
