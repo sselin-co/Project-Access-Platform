58bf6030cfcde69773846815f22c4368d4c8d133
Xinyi Guo <xguo99@mit.edu>
2021-01-01 14:51:46 +0800

    admin gives feedback to student work

--- pages/api/admin/accepted-students/[id].js
+++ pages/api/admin/accepted-students/[id].js
@@ -1,9 +1,12 @@
 import { getSession } from 'next-auth/client'
+import Admin from '../../utils/Admin.js';
 import Student from "../../utils/Student.js"
-
 /**
  * (admin only) get detailed info about an accepted student by id
  * @name GET /api/admin/accepted-students/{studentid}
+ * 
+ * (admin only) give feedback to student assignment
+ * @name POST /api/admin/accepted-students/{studentid}
  */
 export default async (req, res) => {
   const session = await getSession({ req })
@@ -14,9 +17,15 @@ export default async (req, res) => {
             query: { id },
         } = req;
 
-        const student = await Student.oneAccepted(id);
-        console.log(student);
-        res.status(200).json({student: student, message: `info for student ${id} loaded`});
+        if (req.method === 'GET'){
+            const student = await Student.oneAccepted(id);
+            console.log(student);
+            res.status(200).json({student: student, message: `info for student ${id} loaded`});
+        } else if (req.method === 'POST'){
+            const { module_number, feedback } = req.body;
+            await Admin.giveFeedback(id, module_number, feedback);
+            res.status(200).json({message: `Successfully given feedback to student ${id}`});
+        }
     } catch (err) {
         console.log("error", err);
         res.status(400).json({error: err});
