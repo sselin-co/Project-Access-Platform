d1124cc0521a0c83e92687b081a4617c4cb05861
Emmanuel <immanuelidun10@gmail.com>
2021-01-02 04:57:31 -0600

    modified footer

--- pages/general/footer.js
+++ pages/general/footer.js
@@ -4,13 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faLinkedin,
     faFacebook,
-    faInstagram
+    faInstagram,
+    faTwitter,
+    faYoutube
 } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 library.add(
     faLinkedin,
     faFacebook,
     faInstagram,
+    faTwitter,
+    faYoutube,
     faEnvelope
 );
 import styles from "../../styles/Home.module.css";
@@ -22,10 +26,12 @@ export default function Footer() {
         <footer className={styles.footer}>
             <div >
                 <div className="row text-center">
-                    <div className="col-md-3"><a target="_blank" href="https://www.facebook.com/projectaccessaustria"><FontAwesomeIcon style={{ width: '40px' }} icon={faFacebook} /></a></div>
-                    <div className="col-md-3"><a target="_blank" href="https://www.linkedin.com/company/projectaccess/"><FontAwesomeIcon style={{ width: '40px' }} icon={faLinkedin} /></a></div>
-                    <div className="col-md-3"><a target="_blank" href="https://www.instagram.com/projectaccess.at/"><FontAwesomeIcon style={{ width: '40px' }} icon={faInstagram} /></a></div>
-                    <div className="col-md-3"><a target="_blank" href="mailto:hi@projectaccess.at"><FontAwesomeIcon style={{ width: '40px' }} icon={faEnvelope} /></a></div>
+                    <div className="col-md-2"><a target="_blank" href="https://www.facebook.com/projectaccessaustria"><FontAwesomeIcon style={{ width: '40px' }} icon={faFacebook} /></a></div>
+                    <div className="col-md-2"><a target="_blank" href="https://www.linkedin.com/company/projectaccess/"><FontAwesomeIcon style={{ width: '40px' }} icon={faLinkedin} /></a></div>
+                    <div className="col-md-2"><a target="_blank" href="https://www.youtube.com/channel/UCgyGgt6l17sLAvXTFoaQOdA"><FontAwesomeIcon style={{ width: '40px' }} icon={faYoutube} /></a></div>
+                    <div className="col-md-2"><a target="_blank" href="https://twitter.com/projectaccess_"><FontAwesomeIcon style={{ width: '40px' }} icon={faTwitter} /></a></div>
+                    <div className="col-md-2"><a target="_blank" href="https://www.instagram.com/projectaccess.at/"><FontAwesomeIcon style={{ width: '40px' }} icon={faInstagram} /></a></div>
+                    <div className="col-md-2"><a target="_blank" href="mailto:hi@projectaccess.at"><FontAwesomeIcon style={{ width: '40px' }} icon={faEnvelope} /></a></div>
                 </div>
                 <p id="copyright">&copy; 2020 | Project Access Austria</p>
 
