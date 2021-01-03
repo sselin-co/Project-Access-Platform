d3969a58576ec03a2bed9f9ad1eb31d88844ec73
Xinyi Guo <xguo99@mit.edu>
2021-01-03 14:10:46 +0800

    homework reminder email

--- pages/api/admin/accepted-students/[id].js
+++ pages/api/admin/accepted-students/[id].js
@@ -1,11 +1,15 @@
-import { getSession } from 'next-auth/client'
+import { getSession } from 'next-auth/client';
 import Admin from '../../utils/Admin.js';
-import Student from "../../utils/Student.js"
+import Student from "../../utils/Student.js";
+import Mailer from '../../utils/Mailer.js';
 /**
  * (admin only) get detailed info about an accepted student by id
  * @name GET /api/admin/accepted-students/{studentid}
  * 
  * (admin only) give feedback to student assignment
+ * @name POST /api/admin/accepted-students/{studentid}
+ * 
+ * (admin only) send student email reminder
  * @name POST /api/admin/accepted-students/{studentid}
  */
 export default async (req, res) => {
@@ -23,8 +27,14 @@ export default async (req, res) => {
             res.status(200).json({student: student, message: `info for student ${id} loaded`});
         } else if (req.method === 'POST'){
             const { module_number, feedback } = req.body;
-            await Admin.giveFeedback(id, module_number, feedback);
-            res.status(200).json({message: `Successfully given feedback to student ${id}`});
+            if (module_number && feedback){
+              await Admin.giveFeedback(id, module_number, feedback);
+              res.status(200).json({message: `Successfully given feedback to student ${id}`});
+            } else{
+              const email = await Student.getEmail(id);
+              await Mailer.mail(email, `Reminder: Project Access Homework`, `Your coach from Project Access is reminding you that one of your module deadline is approaching.`);
+              res.status(200).json({message: `Successfully sent reminder email to student ${id}`});
+            }
         }
     } catch (err) {
         console.log("error", err);
