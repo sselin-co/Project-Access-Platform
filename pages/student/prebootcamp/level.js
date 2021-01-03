3ebb142dcd52b3d413d0c1298fbfbdf29be5ab0e
Emmanuel <immanuelidun10@gmail.com>
2021-01-03 17:52:12 -0600

    feedback review

--- pages/student/prebootcamp/level.js
+++ pages/student/prebootcamp/level.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-// import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import ReviewModal from '../../../components/review-modal';
 // import useSwr from "swr";
 // import styles from "../../../styles/Home.module.css";
 import Card from 'react-bootstrap/Card';
@@ -10,16 +10,29 @@ import Loading from "../../../components/loading";
 
 
 export default function Levels(props) {
+    const [modalShow, setModalShow] = useState(false);
+    const [feedback, setFeedback] = useState("");
+   
+    useEffect(() => {
+        if(props.review)
+        setFeedback(props.review);
+    })
+
 
     return (
        <>
-            {props.submitted && <Card style={{ width: '18rem' }}>
+            {props.submitted && 
+            (<Card style={{ width: '18rem' }}>
                 <Card.Body>
                     <Card.Title>{props.title}</Card.Title>
                     <Card.Subtitle className="mb-2">Deadline - {props.deadline}</Card.Subtitle>
-                    <Card.Link href={props.review}>Review Feedback</Card.Link>
+                    <Card.Link  onClick={() => {
+                        setModalShow(true);
+                    }}>Review Feedback</Card.Link>
                 </Card.Body>
-            </Card>}
+            </Card>
+            )
+            }
 
             {!props.submitted && <Card style={{ width: '18rem' }}>
                 <Card.Body>
@@ -28,6 +41,16 @@ export default function Levels(props) {
 
                 </Card.Body>
             </Card>}
+
+            <ReviewModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                review={feedback}
+                size="sm"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                backdrop="static"
+            />
 
         </>
 
