76c6fc3d7ac983919346a96fc669a4c492246ab8
Shaya Selincourt <sselincourt@gmail.com>
2021-01-03 03:52:34 -0800

    Basic admin course builder functionality has been implemented. Requires formatting and editing of deadlines

--- components/course-builder.js
+++ components/course-builder.js
@@ -1,21 +1,65 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 import { Container, Col, Row, Button } from "react-bootstrap";
 import useSwr from "swr";
 import Loading from "../components/loading";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import CourseContentEditor from "../components/course-content-editor";
+import { BsX } from "react-icons/bs";
+import ContentEditable from "react-contenteditable";
+// import CourseContentEditor from "../components/course-content-editor";
 
 export default function CourseBuilderComponent(props) {
+  const style = {
+    marginLeft: "1rem",
+  };
+
+  const [editorDisabled, setEditorDisabled] = useState(true);
   const [buttonPressed, setButtonPressed] = useState(false);
+  const [buttonText, setButtonText] = useState("Edit Course Content");
+  const [buttonVariant, setButtonVariant] = useState("warning");
   const router = useRouter();
   const courseId = router.query.id ? router.query.id : props.id;
-  const { data, error } = useSwr(
-    `../../api/admin/learning-modules/${courseId}`
-  );
-  if (error) return <div>Failed to load applicant information</div>;
-  if (!data) return <Loading />;
+  const url = `../../api/admin/learning-modules/${courseId}`;
+  const { data, error } = useSwr(url);
+  try {
+    if (!data) return <Loading />;
+  } catch (err) {
+    if (error) return <div>Failed to load applicant information</div>;
+  }
+
+  var text;
+
+  const courseContent = data.course.content;
+
+  const CourseContentEditor = () => {
+    text = useRef(courseContent);
+    let contentEditable = React.createRef();
+    const handleChange = (event) => {
+      text.current = event.target.value;
+    };
+
+    const handleBlur = () => {
+      // console.log(text.current);
+      console.log(text);
+      console.log("Updated: " + courseContent);
+    };
+    return (
+      <div>
+        {" "}
+        <ContentEditable
+          className={styles.editable}
+          innerRef={contentEditable}
+          html={text.current} // innerHTML of the editable div
+          disabled={false} // use true to disable editing
+          onChange={handleChange} // handle innerHTML change
+          tagName="article" // Use a custom HTML tag (uses a div by default)
+          onBlur={handleBlur}
+          disabled={editorDisabled}
+        />
+      </div>
+    );
+  };
 
   return (
     <>
@@ -39,14 +83,61 @@ export default function CourseBuilderComponent(props) {
             </h5>
           </Col>
           <Col>
-            <Button variant="warning">Edit</Button>
+            <Button
+              variant={buttonVariant}
+              onClick={() => {
+                setButtonPressed(!buttonPressed);
+                if (!buttonPressed) {
+                  setButtonText("Click to Confirm Edits");
+                  setButtonVariant("success");
+                  setEditorDisabled(false);
+                } else {
+                  setButtonText("Edit Course Content");
+                  setButtonVariant("warning");
+                  setEditorDisabled(true);
+                  const updatedData = {
+                    content: text.current,
+                  };
+                  console.log(updatedData);
+                  console.log("Updated" + updatedData.content);
+                  const otherParam = {
+                    method: "PUT",
+                    headers: {
+                      "Content-Type": "application/json; charset=UTF-8",
+                    },
+                    body: JSON.stringify(updatedData),
+                  };
+                  fetch(url, otherParam)
+                    .then((response) => response.json())
+                    .then((data) => console.log(data)) // Manipulate the data retrieved back, if we want to do something with it
+                    .catch((err) => console.log(err)); // Do something with the error
+                  setTimeout(function () {
+                    location.reload();
+                  }, 1000);
+                }
+              }}
+            >
+              {buttonText}
+            </Button>
+            {buttonPressed ? (
+              <Button
+                onClick={() => {
+                  setButtonPressed(false);
+                  setEditorDisabled(true);
+                  setButtonText("Edit Course Content");
+                  setButtonVariant("warning");
+                }}
+                style={style}
+                variant="outline-danger"
+              >
+                <BsX />
+              </Button>
+            ) : null}
           </Col>
         </Row>
       </Container>
       <hr className="w-100" />
-      <CourseContentEditor
-        courseContent={data.course.content}
-      ></CourseContentEditor>
+      <CourseContentEditor></CourseContentEditor>
     </>
   );
 }
--- pages/api/admin/learning-modules/[id].js
+++ pages/api/admin/learning-modules/[id].js
@@ -1,52 +1,55 @@
-import { getSession } from 'next-auth/client'
-import Course from "../../utils/Course.js"
+import { getSession } from "next-auth/client";
+import Course from "../../utils/Course.js";
 
 /**
  * (admin only) see detailed info about a course by its id
  * @name GET api/admin/learning-modules/{courseid}
- * 
+ *
  * (admin only) update information about a course by its id
  * @name PUT api/admin/learning-modules/{courseid}
  */
 export default async (req, res) => {
-  const session = await getSession({ req })
+  const session = await getSession({ req });
 
   if (session && session.user.name === "admin") {
-    try{
-        const {
-            query: { id },
-        } = req;
+    try {
+      const {
+        query: { id },
+      } = req;
 
-        if (req.method === 'PUT'){
-            const { content, deadline, assignment, title } = req.body;
-
-            if (title !== undefined){
-                await Course.updateTitle(id, title);
-            }
-
-            if (content !== undefined){
-                await Course.updateContent(id, content);
-            }
-
-            if (deadline !== undefined){
-                await Course.updateDeadline(id, deadline);
-            }
-
-            if (assignment !== undefined){
-                await Course.updateAssignment(id, assignment);
-            }
-
-            res.status(200).json({message:`course ${id} updated successfully.`});
+      if (req.method === "PUT") {
+        const { content, deadline, assignment, title } = req.body;
+        if (title !== undefined) {
+          await Course.updateTitle(id, title);
         }
-        else if (req.method === 'GET'){
-            const course = await Course.getCourse(id);
-            res.status(200).json({course: course});
+
+        if (content !== undefined) {
+          await Course.updateContent(id, content);
         }
+
+        if (deadline !== undefined) {
+          await Course.updateDeadline(id, deadline);
+        }
+
+        if (assignment !== undefined) {
+          await Course.updateAssignment(id, assignment);
+        }
+
+        res.status(200).json({
+          message: `course ${id} updated successfully.`,
+        });
+      } else if (req.method === "GET") {
+        const course = await Course.getCourse(id);
+        res.status(200).json({ course: course });
+      }
     } catch (err) {
-        console.log("error", err);
-        res.status(400).json({error: err});
+      console.log("error", err);
+      res.status(400).json({ error: err });
     }
   } else {
-    res.send({ error: 'You must be sign in as an admin to view the protected content on this page.' })
+    res.send({
+      error:
+        "You must be sign in as an admin to view the protected content on this page.",
+    });
   }
-}
+};
