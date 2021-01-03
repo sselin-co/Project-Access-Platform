b21442e9374fce3de69be50fc205127e371acf1f
Emmanuel <immanuelidun10@gmail.com>
2021-01-03 17:54:21 -0600

    modified prebootcamp page

--- pages/student/prebootcamp/[email].js
+++ pages/student/prebootcamp/[email].js
@@ -3,11 +3,14 @@ import Head from "next/head";
 import useSwr from "swr";
 import { useRouter } from "next/router";
 import Image from 'next/image';
+import {
+    Media
+} from "react-bootstrap";
 import styles from "../../../styles/Home.module.css";
 import PaNavbarStudent from "../../../components/pa-navbar-student.js";
 import Footer from "../../general/footer";
-import Content from "../content";
-import Card from 'react-bootstrap/Card';
+// import Content from "../content";
+// import Card from 'react-bootstrap/Card';
 import Student from "../../api/utils/Student";
 import Levels from "./level";
 import Loading from "../../../components/loading";
@@ -23,11 +26,22 @@ const info = (item, d) => {
     return dead;
 }
 
+const infor = (item, d) => {
+    var dead;
+    //for (let key in item) {
+        //let value = item[key];
+        dead = item[`${d}`];
+   // }
+    return dead;
+}
+
 export default function PreBootcamp() {
     const router = useRouter();
     const studentEmail = router.query.email;
     const [uid, setUid] = useState("");
+    const [nextModule, setNextModule] = useState("");
     const [titles, setTitles] = useState([]);
+    const [contents, setContents] = useState([]);
     const [deadlines, setDeadlines] = useState([]);
     const [accepted, setAccepted] = useState("");
     const { data, error } = useSwr(`/api/student/learning-modules`, fetcher);
@@ -50,11 +64,22 @@ export default function PreBootcamp() {
             })
         }
 
+        if(accepted) {
+            let v;
+            for(v = 1; v <= 6; v++)
+            {
+                if(!infor(accepted, `module_${v}`)) break;
+            }
+            //console.log(v);
+            setNextModule(v);
+        }
+
         if (data) {
             setDeadlines(info(data, "deadline"));
             setTitles(info(data, "title"));
+            setContents(info(data, "content"));
         }
-    }, [data, uid])
+    }, [data, uid, accepted])
 
     return (
         <div className={styles.container}>
@@ -63,15 +88,51 @@ export default function PreBootcamp() {
                 <link rel="icon" href="/logo_key_colour_highres.ico" />
             </Head>
 
+            
+
             {/* <PaNavbar /> */}
             <PaNavbarStudent email={studentEmail}></PaNavbarStudent>
             <h3 className={styles.title}>Pre-Bootcamp</h3>
+
+            <Media className={styles.med}>
+                <img
+                    width={64}
+                    height={64}
+                    className="mr-3"
+                    src="/PA_Rocket_2.png"
+                    alt="Generic placeholder"
+                />
+                <Media.Body>
+                    <h5>Keep progressing!</h5>
+                    <p>
+                        Continue working on Level {nextModule}
+                    </p>
+                </Media.Body>
+            </Media>
+            {/* <Container className="text-center">
+                <CardColumns>
+                    <Card>
+                        <Card.Img variant="top" width={90}
+                            height={200} src="/PA_Rocket_2.png" />
+                    </Card>
+                    <Card className="p-3">
+                        <blockquote className="blockquote mb-0 card-body">
+                            <p>
+                                Keep progressing!
+                            </p>
+                            <p>
+                                Continue working on Level {nextModule}
+                            </p>
+                        </blockquote>
+                    </Card>
+                </CardColumns>
+            </Container> */}
             <main className={styles.main}>
                 {
                    data &&
                      
                         //(<Levels submitted={accepted[`module_${i + 1}`]} title={item.title} review={accepted[`feedback_${i + 1}`]} deadline={item.deadline}></Levels>))
-                    titles.map((t, i) => (<Levels submitted={accepted[`module_${i + 1}`]} title={t} review={accepted[`feedback_${i + 1}`]} deadline={deadlines[i]}></Levels>))
+                    titles.map((t, i) => (<Levels key={t} submitted={accepted[`module_${i + 1}`]} title={t} review={accepted[`feedback_${i + 1}`]} content={contents[i]} deadline={deadlines[i]}></Levels>))
                     
                     
                 }
