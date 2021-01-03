eaa0b8f2b672128335274070454ac569065a17e1
Xinyi Guo <xguo99@mit.edu>
2021-01-03 12:30:34 +0800

    env var airtable.js

--- pages/api/utils/Airtable.js
+++ pages/api/utils/Airtable.js
@@ -7,7 +7,7 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.AIRTABLE_BASE_ID
 );
 
-const table = base("Application");
+const table = base(process.env.AIRTABLE_TABLE_NAME);
 
 const getMinifiedDashboardRecord = (record) => {
   return {
