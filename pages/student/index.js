26a128ecc4c470efe39b43c2dd6c6cb66219a968
Emmanuel <immanuelidun10@gmail.com>
2021-01-02 19:35:22 -0600

    modified home page

--- pages/student/index.js
+++ pages/student/index.js
@@ -3,22 +3,10 @@ import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import PaNavbarStudent from "../../components/pa-navbar-student.js";
 import Footer from "../general/footer";
+import Carousel from 'react-bootstrap/Carousel';
 
 
 export default function StudentHome(props) {
-    //const { data, error } = useSwr(`/api/applicant-info/${uid}`, fetcher);
-    // const [uid, setUid] = useState("");
-
-    // useEffect(() => {
-    //     Student.nameReturn(props.email, "id").then((data) => {
-    //         setUid(data);
-    //     });
-    //     //console.log(uid);
-    // }) 
-
-    // if (error) return <div>Failed to load applicant information</div>;
-    // if (!data) return <Loading />;
-
     return (
         <div className={styles.container}>
             <Head>
@@ -30,9 +18,68 @@ export default function StudentHome(props) {
             <PaNavbarStudent email={props.email}></PaNavbarStudent>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Project Access Austria</h1>
-                {/* <DisplayApplicantInfo></DisplayApplicantInfo> */}
+                {/* <h1 className={styles.title}>Project Access Austria</h1> */}
+                <Carousel>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100 h-100"
+                            width={100}
+                            height={100}
+                            src="/1500x500.jpg"
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100 h-100"
+                            width={100}
+                            height={100}
+                            src="/1595842023397.jpg"
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100 h-100"
+                            width={100}
+                            height={100}
+                            src="/1598970833919.jpg"
+                            alt="Third slide"
+                        />
+
+                    </Carousel.Item>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100 h-100"
+                            width={100}
+                            height={100}
+                            src="/1606741747667.jpg"
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
+
+
             </main>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/wQf4C9AL1k0" 
+            frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullccreen></iframe>
+
+            <div className="col work-col">
+                <div className="small-12 medium-5 medium-pull-7 columns">
+                    <div className="details details-ftm">
+                        <h3 className="text-center">Press article about Project Access Austria</h3>
+                        <a href="https://m.noen.at/baden/auslandsstudium-von-baden-in-die-weite-welt-baden-auslandsstudium-217784883">From Baden to the big wide world - NÖN.at</a><br />
+                        <a href="https://m.noen.at/moedling/moedling-neue-herausforderung-unistudium-im-ausland-moedling-auslandsstudium-studium-217793508">New challenge: University studies abroad - NÖN.at</a><br />
+                        <a href="https://www.diepresse.com/5494575/oxford-cambridge-und-co-top-unis-fur-alle#">Oxford, Cambridge and Co. - Top universities for everyone - DiePresse</a><br />
+                        <a href="https://www.meinbezirk.at/klosterneuburg/c-lokales/brg-maturantin-auf-bestem-weg-nach-grossbritannien_a2915174">BRG high school graduate on the best way to Great Britain - my district.at</a><br />
+                        <a href="https://www.pressreader.com/austria/der-standard/20190720/282011853953461">Preparation for studying at a top university - the standard</a><br />
+                        <a href="https://kurier.at/kiku/trotz-corona-covid19-und-brexit-wie-gehts-hier-zu-oxbridge/400849982">How's it going to Oxbridge? - courier</a><br />
+                        <a href="https://weltweitestars.com/lifestyle/so-klappts-mit-dem-studium-im-ausland/">This is how it works with studying abroad - WorldwideSTARS</a><br />
+                        <a href="https://www.lsr-ooe.gv.at/fileadmin/erlasssammlung/2018/B9-21-4-18.pdf">State School Board</a><br />
+                    </div>
+                </div>
+            </div>
 
             <Footer/>
         </div>
