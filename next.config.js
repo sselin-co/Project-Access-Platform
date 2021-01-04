988d1d5b04226caf285267a0928d3323d4f52d5f
Shaya Selincourt <sselincourt@gmail.com>
2021-01-04 02:37:07 -0800

    Added all .env.local variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -13,5 +13,7 @@ module.exports = {
   env: {
     AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
     AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
+    AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
 };
